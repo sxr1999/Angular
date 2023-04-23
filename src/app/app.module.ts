36a960a5ffa08b9b217163ecfdff9eecf4d52fc5
sxr1999 <shenxinran1999@gmail.com>
2023-04-23 23:57:45 +0800

    添加了httpclient post方法

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,8 @@ import { MydatePipe } from './mydate.pipe';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/myc02/myc02.component.ts
+++ src/app/myc02/myc02.component.ts
@@ -10,7 +10,8 @@ import { Data } from '@angular/router';
 export class Myc02Component implements OnInit{
 
   constructor(public http : HttpClient){
-
+    this.Model = { date: new Date(), summary: '', temperatureC: 0, temperatureF: 0 };
+    
   }
 
   Result:Res[]
@@ -19,18 +20,35 @@ export class Myc02Component implements OnInit{
   ngOnInit(): void {
 
 
+    this.GetData();
+  }
+
+  GetData(){
     this.http.get<Res[]>('/api/WeatherForecast').subscribe(res=>{
       console.log(res)
       this.Result = res
-
-      
-  
     })
+  }
+
+  Model: Res;
+
+ postCfg = {
+    headers: {'Content-Type': 'application/json;charset=UTF-8'}
+  };
+
+  add(){
+    console.log(this.Model)
+    this.http.post('/api/WeatherForecast',this.Model,this.postCfg,).subscribe(res=>{
+       this.GetData();
+    })
+    
   }
 }
 
+
+
 interface Res{
-  data : Date;
+  date : Date;
   summary : string;
   temperatureC : Number;
   temperatureF : Number
